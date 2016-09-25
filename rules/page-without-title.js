8b87f2ce43442b306ee0631b8549508a5f04e15b
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2016-09-25 18:43:23 +0200

    Add rule page-without-title

--- rules/page-without-title.js
+++ rules/page-without-title.js
@@ -3,10 +3,12 @@ const cheerio = require('cheerio');
 module.exports = function(html, url){
   const $ = cheerio.load(html);
 
-  if($('head title').length == 0){
-      return {
-          level: 'error',
-          message: `Your page ${url} should have a title`
-      };
+  if($('head').length > 0){
+    if($('head title').length == 0 || $('head').text() == ''){
+        return {
+            level: 'error',
+            message: `Your page ${url} should have a title`
+        };
+    }
   }
 };
