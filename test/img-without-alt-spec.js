f6dd7e76c2c2e315763fe875483d9510d9af4c27
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2016-09-25 18:43:40 +0200

    Review test for img-without-alt rule

--- test/img-without-alt-spec.js
+++ test/img-without-alt-spec.js
@@ -1,20 +1,20 @@
 const assert = require('assert');
 const rule = require('../rules/img-without-alt');
 
-describe("Image Without Alt ", function() {
+describe("img-without-alt rule ", function() {
   it('should not throw an error for <div></div>', function() {
     let message = rule('<div></div>', 'http://localhost.com');
     assert(!message);
   });
 
-  it('should not throw an error for <img />', function() {
+  it('should throw an error for <img />', function() {
     let message = rule('<img />', 'http://localhost.com');
     assert(message);
     assert.ok(message.level, "error");
     assert.ok(message.message, "You have images without alt attributes at http://localhost.com");
   });
 
-  it('should not throw an error for <img alt="" />', function() {
+  it('should throw an error for <img alt="" />', function() {
     let message = rule('<img alt="" />', 'http://localhost.com');
     assert(message);
     assert.ok(message.level, "error");
